823d5877d3fef745ee4d3dd65348402c86a8a2ec
Tales Buzanello <69158760+TalesBuzanello@users.noreply.github.com>
2025-08-19 21:23:14 -0300

    Create vaiculo.php

---
+++ aula6/exercicio1/vaiculo.php
@@ -0,0 +1,34 @@
+<!--
+1- Incremente o programa de “Dados do Veículo” do slide anterior.
+Para o combustível, mostrar “Álcool”, “Gasolina” e “Flex” quando os
+valores forem “A”, “G” e “F”, respectivamente.
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Veiculo</title>
+</head>
+
+<body>
+    <h2>Dados do Veículo</h2> <!-- veiculo_form.php -->
+    <form method="POST" action="veiculo_exec.php">
+        <input name="modelo" placeholder="Informe o modelo" />
+        <br><br>
+        <input name="marca" placeholder="Informe a marca" />
+        <br><br>
+        <select name="combustivel">
+            <option value="">--Selecione o combustível---</option>
+            <option value="A">Álcool</option>
+            <option value="G">Gasolina</option>
+            <option value="F">Flex</option>
+        </select>
+        <br><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+
+</html>
