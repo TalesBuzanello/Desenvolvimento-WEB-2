3166c4f4dc55b7c41c5fedf1d7306eb381a9c56b
Tales Buzanello <69158760+TalesBuzanello@users.noreply.github.com>
2025-08-19 21:19:51 -0300

    Create formulario.php

---
+++ aula6/formulario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario HTML</title>
+</head>
+<body>
+
+  <h1>Formulário</h1>
+
+  <form action="processa.php" method="POST">
+    <input type="text" placeholder="Informe o nome" name="nome">
+
+    <br><br>
+
+    <input type="number" placeholder="Informe a idade" name="idade">
+
+    <br><br>
+
+    <button type="submit">Enviar</button>
+
+
+  </form>
+    
+</body>
+</html>
